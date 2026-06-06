3e6f67b040644a28d64b4b66be7101cba2953afb
eivmosn <eivmosn@163.com>
2026-06-07 00:32:08 +0800

    refactor(renderer): remove div debug mode

--- packages/renderer/src/adapters/naive-ui/components/Div.vue
+++ packages/renderer/src/adapters/naive-ui/components/Div.vue
@@ -10,25 +10,14 @@ const props = defineProps<UiNodeProps>()
 const attrs = useAttrs()
 
 const divProps = computed(resolveDivProps)
-const shouldShowDebugState = computed(resolveDebugStateVisible)
 
 /**
  * 解析容器透传属性。
  *
- * @returns 返回过滤调试配置后的容器属性。
+ * @returns 返回容器透传属性。
  */
 function resolveDivProps(): Record<string, unknown> {
-  const { debug, ...restProps } = props.node.props
-  return mergeProps(attrs, restProps)
-}
-
-/**
- * 解析是否展示调试状态。
- *
- * @returns 返回是否展示当前运行时状态。
- */
-function resolveDebugStateVisible(): boolean {
-  return props.node.props.debug === true
+  return mergeProps(attrs, props.node.props)
 }
 </script>
 
@@ -39,7 +28,6 @@ function resolveDebugStateVisible(): boolean {
   >
     {{ props.node.label }}
     <slot />
-    <pre v-if="shouldShowDebugState">{{ props.context.state }}</pre>
   </div>
 </template>
 
