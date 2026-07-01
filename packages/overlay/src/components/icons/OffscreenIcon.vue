05a21244c2ce5fb484e123109997ec937301ec86
eivmosn <eivmosn@163.com>
2026-07-01 16:46:15 +0800

    fix(overlay): set svg icon aria-hidden

--- packages/overlay/src/components/icons/OffscreenIcon.vue
+++ packages/overlay/src/components/icons/OffscreenIcon.vue
@@ -1,5 +1,13 @@
 <template>
-  <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
+  <svg
+    aria-hidden="true"
+    focusable="false"
+    width="24"
+    height="24"
+    viewBox="0 0 48 48"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
     <path d="M33 6V15H42" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M15 6V15H6" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
     <path d="M15 42V33H6" stroke="currentColor" stroke-width="4" stroke-linecap="round" stroke-linejoin="round" />
